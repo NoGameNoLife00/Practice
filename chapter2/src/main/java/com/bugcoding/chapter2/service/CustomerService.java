019a17f9c0dd1d7c0d7ded6dc7ba1b1edbca010f
NoGameNoLife <NoGameNo_Life@163.com>
2016-10-10 22:49:39 +0800

    数据库操作类

--- chapter2/src/main/java/com/bugcoding/chapter2/service/CustomerService.java
+++ chapter2/src/main/java/com/bugcoding/chapter2/service/CustomerService.java
@@ -1,21 +1,51 @@
 package com.bugcoding.chapter2.service;
 
+import com.bugcoding.chapter2.helper.DatabaseHelper;
 import com.bugcoding.chapter2.model.Customer;
+import com.bugcoding.chapter2.util.PropsUtil;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+import org.apache.log4j.spi.LoggerFactory;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Observable;
+import java.sql.*;
+import java.util.*;
 
 /**
  * Created by tao on 2016/10/7.
  */
 public class CustomerService {
+    private static final Logger LOGGER;
+
+
     /**
      * 获取客户列表
      * @param keyword
      * @return
      */
     public List<Customer> getCustomerList(String keyword) {
+        Connection conn = null;
+        try {
+            List<Customer> customerList = new ArrayList<Customer>();
+            String sql = "SELECT * FROM customer";
+            conn = DatabaseHelper.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                Customer customer = new Customer();
+                customer.setId(rs.getLong("id"));
+                customer.setName(rs.getString("name"));
+                customer.setContact(rs.getString("contact"));
+                customer.setTelephone(rs.getString("telephone"));
+                customer.setEmail(rs.getString("email"));
+                customer.setRemark(rs.getString("remark"));
+                customerList.add(customer);
+            }
+            return customerList;
+        } catch (SQLException e) {
+            LOGGER.error("execute sql failure", e);
+        } finally {
+            DatabaseHelper.closeConnection(conn);
+        }
         return null;
     }
 
