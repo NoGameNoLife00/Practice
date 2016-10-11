f72787429336f1f182d603414bdc572afa4338fe
NoGameNoLife <NoGameNo_Life@163.com>
2016-10-11 22:49:40 +0800

    完善数据库操作类，使用连接池

--- chapter2/src/main/java/com/bugcoding/chapter2/service/CustomerService.java
+++ chapter2/src/main/java/com/bugcoding/chapter2/service/CustomerService.java
@@ -2,10 +2,9 @@ package com.bugcoding.chapter2.service;
 
 import com.bugcoding.chapter2.helper.DatabaseHelper;
 import com.bugcoding.chapter2.model.Customer;
-import com.bugcoding.chapter2.util.PropsUtil;
-import org.apache.log4j.Logger;
-import org.apache.log4j.PatternLayout;
-import org.apache.log4j.spi.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 import java.sql.*;
 import java.util.*;
@@ -14,7 +13,7 @@ import java.util.*;
  * Created by tao on 2016/10/7.
  */
 public class CustomerService {
-    private static final Logger LOGGER;
+    private static final Logger LOGGER = LoggerFactory.getLogger(DatabaseHelper.class);
 
 
     /**
@@ -23,30 +22,8 @@ public class CustomerService {
      * @return
      */
     public List<Customer> getCustomerList(String keyword) {
-        Connection conn = null;
-        try {
-            List<Customer> customerList = new ArrayList<Customer>();
-            String sql = "SELECT * FROM customer";
-            conn = DatabaseHelper.getConnection();
-            PreparedStatement stmt = conn.prepareStatement(sql);
-            ResultSet rs = stmt.executeQuery();
-            while (rs.next()) {
-                Customer customer = new Customer();
-                customer.setId(rs.getLong("id"));
-                customer.setName(rs.getString("name"));
-                customer.setContact(rs.getString("contact"));
-                customer.setTelephone(rs.getString("telephone"));
-                customer.setEmail(rs.getString("email"));
-                customer.setRemark(rs.getString("remark"));
-                customerList.add(customer);
-            }
-            return customerList;
-        } catch (SQLException e) {
-            LOGGER.error("execute sql failure", e);
-        } finally {
-            DatabaseHelper.closeConnection(conn);
-        }
-        return null;
+        String sql = "SELECT * FROM customer";
+        return DatabaseHelper.queryEntityList(Customer.class, sql);
     }
 
     /**
@@ -64,7 +41,7 @@ public class CustomerService {
      * @return
      */
     public boolean createCustomer(Map<String, Object> fieldMap) {
-        return false;
+        return DatabaseHelper.insertEntity(Customer.class, fieldMap);
     }
 
     /**
@@ -74,7 +51,7 @@ public class CustomerService {
      * @return
      */
     public boolean updateCustomer(long id, Map<String, Object> fieldMap) {
-        return false;
+        return DatabaseHelper.updateEntity(Customer.class, id, fieldMap);
     }
 
     /**
@@ -83,6 +60,6 @@ public class CustomerService {
      * @return
      */
     public boolean deleteCustomer(long id) {
-        return false;
+        return DatabaseHelper.deleteEntity(Customer.class, id);
     }
 }
