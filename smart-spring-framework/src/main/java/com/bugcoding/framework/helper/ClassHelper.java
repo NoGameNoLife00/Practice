8838751adb3e48ece526f11d069ba76f5dd67f4b
NoGameNoLife <NoGameNo_Life@163.com>
2016-10-16 23:00:12 +0800

    ClassHelper.java

---
+++ smart-spring-framework/src/main/java/com/bugcoding/framework/helper/ClassHelper.java
@@ -0,0 +1,68 @@
+package com.bugcoding.framework.helper;
+
+import com.bugcoding.framework.ConfigConstant;
+import com.bugcoding.framework.annotation.Controller;
+import com.bugcoding.framework.annotation.Service;
+import com.bugcoding.framework.util.ClassUtil;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 类操作助手
+ */
+public final class ClassHelper {
+    /**
+     * 类集合
+     */
+    private static final Set<Class<?>> CLASS_SET;
+
+    static {
+        String basePackage = ConfigHelper.getAppBasePackage();
+        CLASS_SET = ClassUtil.getClassSet(basePackage);
+    }
+
+    /**
+     * 获取应用包名下所有类
+     * @return
+     */
+    public static Set<Class<?>> getClassSet() {
+        return CLASS_SET;
+    }
+
+    /**
+     * 获取应用包名下所以Service类
+     * @return
+     */
+    public static Set<Class<?>> getServiceClassSet() {
+        Set<Class<?>> classSet = new HashSet<Class<?>>();
+        for (Class<?> cls : CLASS_SET) {
+            if (cls.isAnnotationPresent(Service.class)) {
+                classSet.add(cls);
+            }
+        }
+        return classSet;
+    }
+
+
+    /**
+     * 获取应用包名下所以Controller类
+     * @return
+     */
+    public static Set<Class<?>> getControllerClassSet() {
+        Set<Class<?>> classSet = new HashSet<Class<?>>();
+        for (Class<?> cls : CLASS_SET) {
+            if (cls.isAnnotationPresent(Controller.class)) {
+                classSet.add(cls);
+            }
+        }
+        return classSet;
+    }
+
+    public static Set<Class<?>> getBeanClassSet() {
+        Set<Class<?>> beanClassSet = new HashSet<Class<?>>();
+        beanClassSet.addAll(getServiceClassSet());
+        beanClassSet.addAll(getControllerClassSet());
+        return beanClassSet;
+    }
+}
