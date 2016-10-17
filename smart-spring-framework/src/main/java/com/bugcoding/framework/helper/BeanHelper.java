49e953754839ef4293ad0fa54a7fa72d9c295f02
NoGameNoLife <NoGameNo_Life@163.com>
2016-10-17 21:32:59 +0800

    Bean 反射类

---
+++ smart-spring-framework/src/main/java/com/bugcoding/framework/helper/BeanHelper.java
@@ -0,0 +1,36 @@
+package com.bugcoding.framework.helper;
+
+import com.bugcoding.framework.util.ReflectionUtil;
+import com.sun.xml.internal.messaging.saaj.packaging.mime.util.BEncoderStream;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Bean助手类
+ */
+public final class BeanHelper {
+
+    private static final Map<Class<?>, Object> BEAN_MAP = new HashMap<Class<?>, Object>();
+
+    static {
+        Set<Class<?>> beanClassSet = ClassHelper.getBeanClassSet();
+        for (Class<?> beanClass : beanClassSet) {
+            Object obj = ReflectionUtil.newInstance(beanClass);
+            BEAN_MAP.put(beanClass, obj);
+        }
+    }
+
+    public static Map<Class<?>, Object> getBeanMap() {
+        return BEAN_MAP;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> T getBean(Class<T> cls) {
+        if (!BEAN_MAP.containsKey(cls)) {
+            throw new RuntimeException("can not get bean by class:" + cls);
+        }
+        return (T) BEAN_MAP.get(cls);
+    }
+}
