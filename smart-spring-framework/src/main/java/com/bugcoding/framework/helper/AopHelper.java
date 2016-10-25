a24f42ce9259e0c07e6e34c7a971f18457138073
NoGameNoLife <NoGameNo_Life@163.com>
2016-10-25 22:49:40 +0800

    Aop助手类

---
+++ smart-spring-framework/src/main/java/com/bugcoding/framework/helper/AopHelper.java
@@ -0,0 +1,8 @@
+package com.bugcoding.framework.helper;
+
+/**
+ * 方法拦截助手
+ */
+public final class AopHelper {
+    
+}
--- smart-spring-framework/src/main/java/com/bugcoding/framework/helper/ClassHelper.java
+++ smart-spring-framework/src/main/java/com/bugcoding/framework/helper/ClassHelper.java
@@ -5,6 +5,7 @@ import com.bugcoding.framework.annotation.Controller;
 import com.bugcoding.framework.annotation.Service;
 import com.bugcoding.framework.util.ClassUtil;
 
+import java.lang.annotation.Annotation;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -64,5 +65,25 @@ public final class ClassHelper {
         beanClassSet.addAll(getServiceClassSet());
         beanClassSet.addAll(getControllerClassSet());
         return beanClassSet;
+    }
+
+    public static Set<Class<?>> getClassSetBySupper(Class<?> superClass) {
+        Set<Class<?>> classSet = new HashSet<Class<?>>();
+        for (Class<?> cls : CLASS_SET) {
+            if (superClass.isAssignableFrom(cls) && !superClass.equals(cls)) {
+                classSet.add(cls);
+            }
+        }
+        return classSet;
+    }
+
+    public static Set<Class<?>> getClassSetByAnnotation(Class<? extends Annotation>
+                                                                annotationClass) {
+        Set<Class<?>> classSet = new HashSet<Class<?>>();
+        for (Class<?> cls : classSet) {
+            if (cls.isAnnotationPresent(annotationClass))
+                classSet.add(cls);
+        }
+        return classSet;
     }
 }
