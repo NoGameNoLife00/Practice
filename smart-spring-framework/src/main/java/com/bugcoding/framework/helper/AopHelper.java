da51cf7db1eb9d5c5050177c6acb9c41f365df1f
NoGameNoLife <NoGameNo_Life@163.com>
2016-10-26 22:07:38 +0800

    获取映射关系

--- smart-spring-framework/src/main/java/com/bugcoding/framework/helper/AopHelper.java
+++ smart-spring-framework/src/main/java/com/bugcoding/framework/helper/AopHelper.java
@@ -1,8 +1,27 @@
 package com.bugcoding.framework.helper;
 
+import com.bugcoding.framework.annotation.Aspect;
+import com.bugcoding.framework.proxy.AspectProxy;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * 方法拦截助手
  */
 public final class AopHelper {
-    
+
+    private static Map<Class<?>, Set<Class<?>>> createProxyMap() throws Exception {
+        Map<Class<?>, Set<Class<?>>> proxyMap = new HashMap<Class<?>, Set<Class<?>>>();
+        Set<Class<?>> proxyClassSet = ClassHelper.getClassSetBySupper(AspectProxy.class);
+        for (Class<?> proxyClass : proxyClassSet) {
+            if (proxyClass.isAnnotationPresent(Aspect.class)) {
+                Aspect aspect = proxyClass.getAnnotation(Aspect.class);
+                Set<Class<?>> targetClassSet = createTargetClassSet(aspect);
+                proxyMap.put(proxyClass, targetClassSet);
+            }
+        }
+        return proxyMap;
+    }
 }
