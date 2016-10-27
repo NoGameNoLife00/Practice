c6b1663f3ac5571cd5a83914ac5671ec83c0d6c6
NoGameNoLife <NoGameNo_Life@163.com>
2016-10-27 23:00:15 +0800

    AopHelper

--- smart-spring-framework/src/main/java/com/bugcoding/framework/helper/AopHelper.java
+++ smart-spring-framework/src/main/java/com/bugcoding/framework/helper/AopHelper.java
@@ -2,10 +2,9 @@ package com.bugcoding.framework.helper;
 
 import com.bugcoding.framework.annotation.Aspect;
 import com.bugcoding.framework.proxy.AspectProxy;
+import com.bugcoding.framework.proxy.Proxy;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * 方法拦截助手
@@ -23,5 +22,24 @@ public final class AopHelper {
             }
         }
         return proxyMap;
+    }
+
+    private static Map<Class<?>, List<Proxy>> createTargetClassSet(Map<Class<?>, Set<Class<?>>> proxMap) throws Exception {
+        Map<Class<?>, List<Proxy>> targetMap = new HashMap<Class<?>, List<Proxy>>();
+        for (Map.Entry<Class<?>, Set<Class<?>>> proxyEntry : proxMap.entrySet()) {
+            Class<?> proxyClass = proxyEntry.getKey();
+            Set<Class<?>> targetClassSet = proxyEntry.getValue();
+            for (Class<?> targetClass : targetClassSet) {
+                Proxy proxy = (Proxy) proxyClass.newInstance();
+                if (targetMap.containsKey(targetClass)) {
+                    targetMap.get(targetClass).add(proxy);
+                } else {
+                    List<Proxy> proxyList = new ArrayList<Proxy>();
+                    proxyList.add(proxy);
+                    targetMap.put(targetClass, proxyList);
+                }
+            }
+        }
+        return targetMap;
     }
 }
