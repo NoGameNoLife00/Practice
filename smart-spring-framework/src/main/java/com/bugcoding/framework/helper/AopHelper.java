1d129e1fa7d117109bbdf2d955bd5515e6e4912d
NoGameNoLife <NoGameNo_Life@163.com>
2016-10-30 22:28:07 +0800

    AopHelper

--- smart-spring-framework/src/main/java/com/bugcoding/framework/helper/AopHelper.java
+++ smart-spring-framework/src/main/java/com/bugcoding/framework/helper/AopHelper.java
@@ -3,13 +3,34 @@ package com.bugcoding.framework.helper;
 import com.bugcoding.framework.annotation.Aspect;
 import com.bugcoding.framework.proxy.AspectProxy;
 import com.bugcoding.framework.proxy.Proxy;
+import com.bugcoding.framework.proxy.ProxyManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.lang.annotation.Annotation;
 import java.util.*;
 
 /**
  * 方法拦截助手
  */
 public final class AopHelper {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AopHelper.class);
+
+    static {
+        try {
+            Map<Class<?>, Set<Class<?>>> proxyMap = createProxyMap();
+            Map<Class<?>, List<Proxy>> targetMap = createTargetClassSet(proxyMap);
+            for (Map.Entry<Class<?>, List<Proxy>> targetEntery : targetMap.entrySet()) {
+                Class<?> targetClass = targetEntery.getKey();
+                List<Proxy> proxyList = targetEntery.getValue();
+                Object proxy = ProxyManager.createProxy(targetClass, proxyList);
+                BeanHelper.setBean(targetClass, proxy);
+            }
+        } catch (Exception e) {
+            LOGGER.error("aop failure", e);
+        }
+    }
 
     private static Map<Class<?>, Set<Class<?>>> createProxyMap() throws Exception {
         Map<Class<?>, Set<Class<?>>> proxyMap = new HashMap<Class<?>, Set<Class<?>>>();
@@ -24,9 +45,18 @@ public final class AopHelper {
         return proxyMap;
     }
 
-    private static Map<Class<?>, List<Proxy>> createTargetClassSet(Map<Class<?>, Set<Class<?>>> proxMap) throws Exception {
+    private static Set<Class<?>> createTargetClassSet(Aspect aspect) throws Exception {
+        Set<Class<?>> targetClassSet = new HashSet<Class<?>>();
+        Class<? extends Annotation> annotation = aspect.value();
+        if (annotation != null && !annotation.equals(Aspect.class)) {
+            targetClassSet.addAll(ClassHelper.getClassSetByAnnotation(annotation));
+        }
+        return targetClassSet;
+    }
+
+    private static Map<Class<?>, List<Proxy>> createTargetClassSet(Map<Class<?>, Set<Class<?>>> proxyMap) throws Exception {
         Map<Class<?>, List<Proxy>> targetMap = new HashMap<Class<?>, List<Proxy>>();
-        for (Map.Entry<Class<?>, Set<Class<?>>> proxyEntry : proxMap.entrySet()) {
+        for (Map.Entry<Class<?>, Set<Class<?>>> proxyEntry : proxyMap.entrySet()) {
             Class<?> proxyClass = proxyEntry.getKey();
             Set<Class<?>> targetClassSet = proxyEntry.getValue();
             for (Class<?> targetClass : targetClassSet) {
