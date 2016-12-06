f4f52e61a04d22bd8c026c2ca6a183226f91d039
NoGameNoLife <NoGameNo_Life@163.com>
2016-12-06 16:57:27 +0800

    二叉树的镜像

---
+++ 剑指offer/二叉树的镜像.cpp
@@ -0,0 +1,42 @@
+// 题目描述
+// 操作给定的二叉树，将其变换为源二叉树的镜像。
+// 输入描述:
+// 二叉树的镜像定义：源二叉树
+//     	    8
+//     	   /  \
+//     	  6   10
+//     	 / \  / \
+//     	5  7 9 11
+//     	镜像二叉树
+//     	    8
+//     	   /  \
+//     	  10   6
+//     	 / \  / \
+//     	11 9 7  5
+
+思路：
+后序遍历树，同时交换遍历到的节点的两个子节点
+
+/*
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+    TreeNode(int x) :
+            val(x), left(NULL), right(NULL) {
+    }
+};*/
+class Solution {
+public:
+    void Mirror(TreeNode *pRoot) {
+        if (pRoot == NULL)
+            return;
+
+        Mirror(pRoot->left);
+        Mirror(pRoot->right);
+        TreeNode *tmp = pRoot->left;
+        pRoot->left = pRoot->right;
+        pRoot->right = tmp;
+        return;
+    }
+};
