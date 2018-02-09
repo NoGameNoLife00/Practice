ee48db1f7b1fe90832e4c98138dc16c3eda8571e
NoGameNoLife <NoGameNo_Life@163.com>
2018-02-09 18:09:10 +0800

    Erlang 代码

--- Erlang程序设计代码/1 你好并发/afile_server.erl
+++ Erlang程序设计代码/1 你好并发/afile_server.erl
@@ -1,4 +1,4 @@
--modlue(afile_server).
+-module(afile_server).
 -export([start/1, loop/1]).
 
 start(Dir) -> spawn(afile_server, loop, [Dir]).
@@ -9,6 +9,10 @@ loop(Dir) ->
 			Client ! {self(), file:list_dir(Dir)};
 		{Client, {get_file, File}} ->
 			Full = filename:join(Dir, File),
-			Client ! {self(), file:read_file(Full)}
+			Client ! {self(), file:read_file(Full)};
+		{Client, {put_file, Content, FileName}} ->
+			FullName = filename:join(Dir, FileName),
+			Result = file:write_file(FullName, Content),
+			Client ! {self(), {Result, FullName, Content}}
 	end,
 	loop(Dir).
